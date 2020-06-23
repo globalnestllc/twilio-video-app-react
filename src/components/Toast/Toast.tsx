47655140683d03bbede85b7f21f4af3ecb5e6878
Hasan Ali Ayar <hasan.a.ayar@gmail.com>
2020-06-23 09:25:31 +0300

    - Improved 2 minutes toast message design.

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -9,6 +9,19 @@ const useStyles = makeStyles(theme => ({
   alertRoot: {
     minWidth: 400,
   },
+  message: {
+    fontSize: '1.5rem',
+  },
+  action: {
+    '& svg': {
+      fontSize: '1.5rem',
+    },
+  },
+  icon: {
+    fontSize: '2rem',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
 }));
 
 function TransitionRight(props: TransitionProps) {
@@ -23,8 +36,18 @@ const Toast = () => {
   };
   const classes = useStyles();
   return (
-    <Snackbar open={toast.open} autoHideDuration={6000} onClose={handleClose} TransitionComponent={TransitionRight}>
-      <Alert variant="filled" severity={toast.severity} onClose={handleClose} className={classes.alertRoot}>
+    <Snackbar open={toast.open} autoHideDuration={15000} onClose={handleClose} TransitionComponent={TransitionRight}>
+      <Alert
+        variant="filled"
+        severity={toast.severity}
+        onClose={handleClose}
+        classes={{
+          root: classes.alertRoot,
+          action: classes.action,
+          message: classes.message,
+          icon: classes.icon,
+        }}
+      >
         {toast.message}
       </Alert>
     </Snackbar>
