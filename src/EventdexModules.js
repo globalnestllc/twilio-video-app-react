bafa578314ad8e8cc0231c89c6d9373ad30050f7
Hasan Ali Ayar <hasan.a.ayar@gmail.com>
2021-05-13 20:32:19 +0300

    Upgraded modules: f97cbbc90194d5b3dda4499f5a1f3a21d90a2531

--- src/EventdexModules.js
+++ src/EventdexModules.js
@@ -5,21 +5,16 @@ import PlayerModule from '@eventdex/player';
 import LoungeModule from '@eventdex/lounge';
 import ScheduleModule from '@eventdex/schedule';
 import EventModule from '@eventdex/event';
-import LiveStreamModule from '@eventdex/liveStream';
 import ChatModule from '@eventdex/chat';
-// import VideoModule from '@eventdex/video'
 import CommonModule from '@eventdex/common';
-import AuthModule from '@eventdex/auth';
 
 registerModule(LoungeModule);
 registerModule(PlayerModule);
 registerModule(ScheduleModule);
 registerModule(EventModule);
-registerModule(LiveStreamModule);
 registerModule(ChatModule);
 registerModule(CoreModule);
 registerModule(CommonModule);
-registerModule(AuthModule);
 
 export default function EventdexModules(props) {
   return <React.Fragment></React.Fragment>;
