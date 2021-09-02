76f2ddcdc610d3cec1aa6313304e2cfa2010f5fb
Hasan Ali Ayar <hasan.a.ayar@gmail.com>
2021-09-02 10:45:25 +0300

    Made survey popup responsive.

--- src/AfterCallSurvey/AfterCallSurveyDialog.js
+++ src/AfterCallSurvey/AfterCallSurveyDialog.js
@@ -5,30 +5,38 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, useTheme } from '@material-ui/core/styles';
 import { urlFor_RestApi } from '@eventdex/core/src/context';
 import { useWindowMessageCallback } from './useWindowMessageCallback';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   dialogPaper: {
     height: '80%',
+    [theme.breakpoints.down('sm')]: {
+      height: '100%',
+    },
   },
-  dialogContent: {
-    textAlign: 'center',
+
+  iframe: {
+    width: '100%',
+    height: '100%',
+    border: 'none',
   },
-});
+}));
 const styles = theme => ({
   root: {
     margin: 0,
-    padding: theme.spacing(2),
+    padding: 0,
   },
   closeButton: {
     position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
+    right: 0,
+    top: 0,
     color: theme.palette.grey[500],
   },
 });
+
 const DialogTitle = withStyles(styles)(props => {
   const { children, classes, onClose, ...other } = props;
   return (
@@ -45,14 +53,9 @@ const DialogTitle = withStyles(styles)(props => {
 
 export default function AfterCallSurveyDialog(props) {
   const classes = useStyles();
-  // const [isOpen,setIsOpen]=useState(true);
   const { open, onClose, url } = props;
-  //
-  // React.useEffect(()=>{
-  //
-  // },[propsOpen])
-  //
-  // const open = propsOpen && isOpen;
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   function handleIframeMessage(message, data) {
     if (data === 'iframe_message' || message === 'iframe_message') {
@@ -64,20 +67,18 @@ export default function AfterCallSurveyDialog(props) {
 
   return (
     <Dialog
+      fullScreen={fullScreen}
       fullWidth={true}
       maxWidth={'md'}
       open={open}
-      // onClose={handleClose}
       aria-labelledby="survey questions"
       classes={{ paper: classes.dialogPaper }}
     >
       <DialogTitle id="max-width-dialog-title" onClose={onClose}>
-        Please answer the following questions
+        {/*Please answer the following questions*/}
       </DialogTitle>
 
-      <DialogContent className={classes.dialogContent}>
-        <iframe src={url} width={'80%'} height={'100%'} style={{ border: 'none' }} />
-      </DialogContent>
+      <iframe src={url} className={classes.iframe} />
     </Dialog>
   );
 }
