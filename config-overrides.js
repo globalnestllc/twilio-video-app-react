3461e8807c0dd8dc275f244ef1a3c6a141ad5e76
Hasan Ali Ayar <hasan.a.ayar@gmail.com>
2022-02-05 10:03:29 +0300

    - fixed build configurations for production.

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,19 @@
 const {override, useBabelRc} = require("customize-cra");
 const {
     circularDependencyPlugin, bundleAnalyzerPlugin, duplicatesPlugin, printConfig,
-    fixModuleResolutionForLinkedPackages, addPolyfills,
+    fixModuleResolutionForLinkedPackages, addPolyfills,devOnly
 } = require("@eventdex/bootstrap/src/hostConfig/config-overrides")
+const path = require("path");
 
 //Add babel aliases to be able to include external code (../eventdex-modules)
-const {aliasDangerous, configPaths} = require("react-app-rewire-alias/lib/aliasDangerous")
-const aliasMap = configPaths("./tsconfig.paths.json") // or jsconfig.paths.json
-const addAliases = aliasDangerous(aliasMap)
+const {aliasDangerous, configPaths} = require("react-app-rewire-alias/lib/aliasDangerous");
+const aliasMap = configPaths("./tsconfig.paths.json"); // or jsconfig.paths.json
+const addAliases = aliasDangerous(aliasMap);
 
 module.exports = {
     webpack: override(
-        addAliases,
+        devOnly(addAliases),
+
         // eslint-disable-next-line react-hooks/rules-of-hooks
         useBabelRc(),
         duplicatesPlugin,
