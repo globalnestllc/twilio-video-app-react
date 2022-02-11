c3925c3753ec323535e36e49627e4dbfab88ac6c
Hasan Ali Ayar <hasan.a.ayar@gmail.com>
2022-02-11 12:27:27 +0300

    use history object from modules.

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
 import store from "./store/store";
 import {initializeContext} from "@eventdex/core/src/hostApp";
-import history from "./history";
 import "react-toastify/dist/ReactToastify.css";
+import history from "@eventdex/bootstrap/src/hostConfig/history";
 
 let hostApp = {
     abbreviation: "vv",
